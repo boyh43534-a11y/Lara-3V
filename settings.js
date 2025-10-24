de06197a84ba9c7c886c675e6d6a3b306e3faccf
boyh43534-a11y <boyh43534@gmail.com>
2025-10-24 10:14:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,12 +5,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "CUAUgbKL#6gong9Za1bJR4UJFCejfw6nXJIaZHBQpGl2ne0Lb0A0", // Add your session id
 PREFIX: process.env.PREFIX || ".",
-MODE: process.env.MODE || "public",  //public | private | inbox | group
-OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
+MODE: process.env.MODE || "Private",  //public | private | inbox | group
+OWNER_NUMBER: process.env.OWNER_NUMBER || "+94760731398",
 DEV: process.env.DEV || "94779062397",
-LANG: process.env.LANG || "EN", // EN | SI
+LANG: process.env.LANG || "SI", // EN | SI
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
